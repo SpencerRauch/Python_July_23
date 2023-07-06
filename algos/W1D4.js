f008294df0c883ed98ecdd3ca0ca121739d31efb
Spencer Rauch <rauchs@gmail.com>
2023-07-06 08:43:25 -0700

    solutions

--- algos/W1D4.js
+++ algos/W1D4.js
@@ -18,16 +18,36 @@ const expected3 = false;
 const str4 = "oho!";
 const expected4 = false;
 
-const str5 = "abba"
+const str5 = "abba";
 const expected5 = true;
 
 
 function isPalindrome(str) {
-    //Your code here
+  //Your code here
 }
 
-console.log(isPalindrome(str1)) //expected: true
-console.log(isPalindrome(str2)) //expected: true
-console.log(isPalindrome(str3)) //expected: false
-console.log(isPalindrome(str4)) //expected: false
-console.log(isPalindrome(str5)) //expected: true
+console.log(isPalindrome(str1)); //expected: true
+console.log(isPalindrome(str2)); //expected: true
+console.log(isPalindrome(str3)); //expected: false
+console.log(isPalindrome(str4)); //expected: false
+console.log(isPalindrome(str5)); //expected: true
+
+
+function isPalindrome(str) {
+  if (str.length < 2) return true;
+  let start = 0;
+  let end = str.length - 1;
+  while (start < end) {
+    if (str[start] !== str[end]) return false;
+    start++;
+    end--;
+  }
+  return true;
+}
+
+
+function isPalindrome2(str) {
+  let str2 = str;
+  str2 = str2.split('').reverse().join('')
+  return str2 === str;
+}
