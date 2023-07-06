23d989f9c8f94bd68147e3c311080d7bf21fafc5
Spencer Rauch <rauchs@gmail.com>
2023-07-06 13:47:09 -0700

    afternoon lecture

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -16,10 +16,19 @@ def get_user(index):
     # index = int(index) another way of accomplishing this
     return list[index]
 
-@app.route('/template')
-def show_temp():
-    return render_template("index.html")
+@app.route('/template/<int:num>')
+def show_temp(num):
+    return render_template("index.html",string="Hello", num=num, color='blue')
 
+@app.route('/my_list')
+def my_list():
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("my_list.html", my_list=student_info)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True,port=5050,host="0.0.0.0")    # Run the app in debug mode
