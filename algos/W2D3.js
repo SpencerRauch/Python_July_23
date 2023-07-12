02d63ba751aca7b14af46989cddddef22589258d
Spencer Rauch <rauchs@gmail.com>
2023-07-12 08:11:35 -0700

    algo

--- algos/W2D3.js
+++ algos/W2D3.js
@@ -32,4 +32,32 @@ console.log(parensValid(str1)) // expected: true
 console.log(parensValid(str2)) // expected: false
 console.log(parensValid(str3)) // expected: false
 
+//
+/* 
+Braces Valid
+Given a string sequence of parentheses, braces and brackets, determine whether it is valid. 
+*/
 
+const strA = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";    // ( {} [ ({}) ] ) [{}]
+const expectedA = true;
+
+const strB = "D(i{a}l[ t]o)n{e";          // ({} []) {
+const expectedB = false;
+
+const strC = "A(1)s[O (n]0{t) 0}k";       // () [(] {) }
+const expectedC = false;
+
+/**
+ * Determines whether the string's braces, brackets, and parenthesis are valid
+ * based on the order and amount of opening and closing pairs.
+
+ * @param {string} str
+ * @returns {boolean} Whether the given strings braces are valid.
+ */
+function bracesValid(str) {
+    //Your code here
+}
+
+console.log(bracesValid(strA)) // expected: true
+console.log(bracesValid(strB)) // expected: false
+console.log(bracesValid(strC)) // expected: false
