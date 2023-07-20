aeecc2d8da4db862dc753fdef4b39b48ca847e1b
Spencer Rauch <rauchs@gmail.com>
2023-07-20 09:34:26 -0700

    ajax review /intro

---
+++ ajax/script.js
@@ -0,0 +1,32 @@
+// alert("Linked!")
+const url = "https://pokeapi.co/api/v2/pokemon/"
+const nameInput  = document.querySelector("#pokeName")
+const resultDiv = document.querySelector("#pokeResult")
+
+function getPoke(event){
+    event.preventDefault();
+    console.log("Form submitted")
+    resultDiv.innerHTML = "Loading....."
+    fetch(url+nameInput.value)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            resultDiv.innerHTML = `
+                <h3> Poke No: ${data.id} Poke Name: ${data.name} </h3>
+                <img src="${data.sprites.front_default}" alt=${data.name}>
+            `
+        })
+        .catch(err=>console.log(err))
+}
+
+async function randPoke(){
+    resultDiv.innerHTML = "Loading....."
+    console.log('clicked rand')
+    let rand = Math.floor(Math.random()*999)
+    let response = await fetch(url + rand)
+    let data = await response.json()
+    resultDiv.innerHTML = `
+    <h3> Poke No: ${data.id} Poke Name: ${data.name} </h3>
+    <img src="${data.sprites.front_default}" alt=${data.name}>
+    `
+}
